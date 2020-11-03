084a7b38d5de7a9d406c77c810fa61d85dd3bf25
chett <chett@DESKTOP-H9N4VAV>
2020-11-03 14:13:00 -0500

    Added board

--- Chess/src/Game/Board.java
+++ Chess/src/Game/Board.java
@@ -1,5 +1,9 @@
 package Game;
 
+import checkerboard.Canvas;
+import checkerboard.CheckerBoard;
+import checkerboard.SquareOld;
+
 public class Board {
 	private Board board;
 	private PieceSet[] pieceSets;
@@ -18,6 +22,44 @@ public class Board {
 	}
 	
 	public void resetBoard() {
+		Board myObj = new Board();
+		myObj.create();
+		System.out.print("need to be able to get the peaces ");
+	}
+	public void startBoard() {
+		//I do the same job as resetBoard but I get my own method OwO
+		resetBoard();
 		
 	}
+	
+	private void create() {
+		frame.getCanvas();
+
+		// TODO: DECLARE A 2D ARRAY OF Square(S) HERE
+		// TODO: MAKE THE ARRAY 8 BY 8
+		final int DIMX = 8;
+		//change mepls
+		final int temp = 40;
+		Square [][] checkerBoard = new Square[DIMX][];
+		//CircleOld [][] checkerBoard = new CircleOld[DIMX][];
+
+		String color;
+		for(int i = 0; i < checkerBoard.length; i++) {
+			checkerBoard[i] =new Square[checkerBoard.length];
+			for(int j = 0; j < checkerBoard[i].length; j++) {
+				if((i + j) % 2 == 1) {
+					color = "black";
+					checkerBoard[i][j] = new Square(temp, j * temp + temp, i * temp + temp, color);
+
+				}
+				else {
+					color = "red";
+					checkerBoard[i][j] = new Square(temp, j * temp + temp, i * temp + temp, color);
+				}
+				
+				checkerBoard[i][j].makeVisible();
+
+			}
+		}
+	} 
 }
--- Chess/src/Game/Square.java
+++ Chess/src/Game/Square.java
@@ -1,6 +1,35 @@
 package Game;
 
+import java.awt.Rectangle;
+
+
+
 public class Square {
 	private int x;
 	private int y;
+	private int size;
+	private boolean isVisible;
+	private String color;
+	
+	Square(int size, int x, int y, String color){
+		this.size = size;
+		this.x = x;
+		this.y = y;
+		this.color = color;
+		
+	}
+
+	protected void draw() {
+	// TODO Auto-generated method stub
+	if(isVisible) {
+		frame canvas = frame.getCanvas();
+		canvas.draw(this, color, new Rectangle(x, y, 40, 40));
+		
+	}
+	}
+	public void makeVisible()
+	{
+		isVisible = true;
+		draw();
+	}
 }
