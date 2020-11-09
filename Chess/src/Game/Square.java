afc92dd1c2461d468107b9a07bd8e7bb282f319c
Colin Truemner <cjaytruemner@gmail.com>
2020-11-09 16:43:38 -0500

    Made the board work hopefully

--- Chess/src/Game/Square.java
+++ Chess/src/Game/Square.java
@@ -1,6 +1,20 @@
 package Game;
 
+import java.awt.Color;
+
 public class Square {
-	private int x;
-	private int y;
+	private Color color;
+	private Piece currPiece;
+	
+	public Square(Color color) {
+		this.color = color;
+		currPiece = null;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
 }
